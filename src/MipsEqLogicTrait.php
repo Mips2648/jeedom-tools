0b74117646d00df6344a89cd400d1cc1731cca17
Mips2648 <mips2648@gmail.com>
2022-01-12 09:10:47 +0100

    add executeAsync

--- src/MipsEqLogicTrait.php
+++ src/MipsEqLogicTrait.php
@@ -19,18 +19,18 @@ trait MipsEqLogicTrait {
 
 	public function createCommandsFromConfig(array $commands) {
 		$link_cmds = array();
-		foreach ($commands as $cmdDef){
+		foreach ($commands as $cmdDef) {
 			$cmd = $this->getCmd(null, $cmdDef["logicalId"]);
 			if (!is_object($cmd)) {
-				log::add(__CLASS__, 'debug', 'create:'.$cmdDef["logicalId"].'/'.$cmdDef["name"]);
+				log::add(__CLASS__, 'debug', 'create:' . $cmdDef["logicalId"] . '/' . $cmdDef["name"]);
 				$cmd = new cmd();
 				$cmd->setLogicalId($cmdDef["logicalId"]);
 				$cmd->setEqLogic_id($this->getId());
 				$cmd->setName(__($cmdDef["name"], __FILE__));
-				if(isset($cmdDef["isHistorized"])) {
+				if (isset($cmdDef["isHistorized"])) {
 					$cmd->setIsHistorized($cmdDef["isHistorized"]);
 				}
-				if(isset($cmdDef["isVisible"])) {
+				if (isset($cmdDef["isVisible"])) {
 					$cmd->setIsVisible($cmdDef["isVisible"]);
 				}
 				if (isset($cmdDef['template'])) {
@@ -41,18 +41,18 @@ trait MipsEqLogicTrait {
 			}
 			$cmd->setType($cmdDef["type"]);
 			$cmd->setSubType($cmdDef["subtype"]);
-			if(isset($cmdDef["generic_type"])) {
+			if (isset($cmdDef["generic_type"])) {
 				$cmd->setGeneric_type($cmdDef["generic_type"]);
 			}
 			if (isset($cmdDef['display'])) {
 				foreach ($cmdDef['display'] as $key => $value) {
-					if ($key=='title_placeholder' || $key=='message_placeholder') {
+					if ($key == 'title_placeholder' || $key == 'message_placeholder') {
 						$value = __($value, __FILE__);
 					}
 					$cmd->setDisplay($key, $value);
 				}
 			}
-			if(isset($cmdDef["unite"])) {
+			if (isset($cmdDef["unite"])) {
 				$cmd->setUnite($cmdDef["unite"]);
 			}
 
@@ -70,7 +70,7 @@ trait MipsEqLogicTrait {
 
 			if (isset($cmdDef['initialValue'])) {
 				$cmdValue = $cmd->execCmd();
-				if ($cmdValue=='') {
+				if ($cmdValue == '') {
 					$this->checkAndUpdateCmd($cmdDef["logicalId"], $cmdDef['initialValue']);
 				}
 			}
@@ -85,5 +85,23 @@ trait MipsEqLogicTrait {
 				$cmd->save();
 			}
 		}
+	}
+
+	private function executeAsync(string $_method, $_option = null) {
+		if (!method_exists(__CLASS__, $_method)) {
+			throw new InvalidArgumentException("Method provided for executeAsync does not exist: {$_method}");
+		}
+
+		$cron = new cron();
+		$cron->setClass(__CLASS__);
+		$cron->setFunction($_method);
+		if (isset($_option)) {
+			$cron->setOption($_option);
+		}
+		$cron->setOnce(1);
+		$cron->setSchedule(cron::convertDateToCron(strtotime("now") - 1));
+		$cron->save();
+		$cron->run();
+		log::add(__CLASS__, 'debug', "ExecuteAsync '{$_method}'");
 	}
 }
